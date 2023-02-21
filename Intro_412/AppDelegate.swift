43792fa3393f01059712bdb688d552d0a70f2c64
MikeDevUcamp <115725749+MikeDevUcamp@users.noreply.github.com>
2023-02-21 17:28:40 -0600

    Initial Commit

---
+++ Intro_412/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Intro_412
+//
+//  Created by Miguel Gómez Díaz on 21/02/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
